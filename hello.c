db0d5a42eaf28c8bb8e4e8624bf7b9e2103ddfeb
shubhamvsnv2 <72275611+shubhamvsnv2@users.noreply.github.com>
2020-10-03 00:26:46 +0530

    Update hello.c
    Improving the code with proper commenting or getch() function

--- hello.c
+++ hello.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
+
+// Header file for input output functions
+#include <stdio.h>
+
+// main function -
+// where the execution of program begins
 void mian()
 {
+// prints hello world and a msg
 printf("hello world...!!");
-printf("this is the basic programmimg");       
+printf("this is the basic programmimg");   
+getch();
 }
